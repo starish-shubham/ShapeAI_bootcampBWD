7ca89b10effef66396008f90ade89f9102bc1eb2
Shubham_starish <85021948+starish-shubham@users.noreply.github.com>
2021-06-06 17:41:28 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  
+  var curryear = new Date().getFullYear();
+
+
+  return (
+    <footer>
+      <p>Copyright @ {curryear}</p>
+    </footer>
+  );
+  
+}
+
+export default Footer;
